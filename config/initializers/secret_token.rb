79989a7b89a5c3fcadf37cb4a0c94b3ce3227def
Federico Pooleye <federicopooley@hotmail.com>
2013-12-23 15:18:46 -0300

    Fist commit and Readme Update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Infopedia::Application.config.secret_token = '98af181fb66bd431dfe2cf4e5ccfb658bb606d6a2a459dc1af91d106b95cf8e62c84444d609413090e16af9027dfaece1ae185dde7197c27cf95f2fe9ea9daea'
